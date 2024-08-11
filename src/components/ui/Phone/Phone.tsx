50c4c1b63f6934a0228b42f5592668cf7237d784
Oleg <ikslprint@gmail.com>
2024-08-11 10:34:01 +0300

    сhange phone number

--- src/components/ui/Phone/Phone.tsx
+++ src/components/ui/Phone/Phone.tsx
@@ -5,7 +5,7 @@ const Phone: React.FC = () => {
     <div className="flex gap-1 items-center">
       <PhoneIcon width={18} height={18} fill="white" className="md:size-5" />
       <a href="tel:+380632790437" className="text-sm font-semibold md:text-lg">
-        (063) 279 04 37
+        (066) 719 60 74
       </a>
     </div>
   );
